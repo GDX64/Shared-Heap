d0cef1600bbb7e31c806a1a96a253111982e8046
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-03-07 20:57:45 -0300

    u64 hash

--- js/src/AnyStore.ts
+++ js/src/AnyStore.ts
@@ -343,13 +343,20 @@ const proxyArraySchema: ProxyHandler<Target> = {
   },
 };
 
-function fastHash(str: string): bigint {
-  let hash = 0;
-  const len = str.length;
-  for (let i = 0; i < len; i++) {
-    hash = ((hash << 5) - hash + str.charCodeAt(i)) | 0;
+export function fastHash(str: string): bigint {
+  // FNV-1a parameters for 64-bit
+  const FNV_OFFSET_BASIS = 0xcbf29ce484222325n;
+  const FNV_PRIME = 0x100000001b3n;
+  const MASK_64 = 0xffffffffffffffffn;
+
+  let hash = FNV_OFFSET_BASIS;
+
+  for (let i = 0; i < str.length; i++) {
+    hash ^= BigInt(str.charCodeAt(i));
+    hash = (hash * FNV_PRIME) & MASK_64;
   }
-  return BigInt(hash >>> 0);
+
+  return hash;
 }
 
 function isArrayID(objID: bigint): boolean {
