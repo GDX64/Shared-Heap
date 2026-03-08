edb2affb2e5065fb1663481f5ab98996c1b34dd7
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-03-08 13:34:13 -0300

    storage

--- src/storage.rs
+++ src/storage.rs
@@ -9,8 +9,19 @@ thread_local! {
     static LOCAL_OBJECTS: RefCell<HashMap<u64, Object>> = RefCell::new(HashMap::new());
 }
 
-fn local_get(id: u64) -> Option<Object> {
-    LOCAL_OBJECTS.with(|objects| objects.borrow().get(&id).cloned())
+fn with_local_object<FLocal, FMissing, R>(id: u64, on_local: FLocal, on_missing: FMissing) -> R
+where
+    FLocal: FnOnce(&Object) -> R,
+    FMissing: FnOnce() -> R,
+{
+    LOCAL_OBJECTS.with(|objects| {
+        let objects = objects.borrow();
+        if let Some(object) = objects.get(&id) {
+            return on_local(object);
+        }
+        drop(objects);
+        on_missing()
+    })
 }
 
 fn local_insert(id: u64, object: Object) {
@@ -37,58 +48,14 @@ pub struct Storage {
     inner: WasmMutex<InnerStorage>,
 }
 
-impl InnerStorage {
-    fn object_id(&mut self) -> u64 {
-        let id = self.last_id;
-        self.last_id += 1;
-        return id << 1;
-    }
-
-    fn array_id(&mut self) -> u64 {
-        let id = self.last_id;
-        self.last_id += 1;
-        return (id << 1) | 0b1;
-    }
-
-    fn cleanup_dead(&mut self, id: u64) {
-        let remove = self
-            .collection
-            .get(&id)
-            .map(|w| w.strong_count() == 0)
-            .unwrap_or(false);
-        if remove {
-            self.collection.remove(&id);
-        }
-    }
-
-    fn get_live_object(&self, id: u64) -> Option<Object> {
-        if let Some(object) = local_get(id) {
-            return Some(object);
-        }
-
-        let weak = self.collection.get(&id)?;
-        let object = weak.upgrade()?;
-        local_insert(id, object.clone());
-        Some(object)
-    }
-
-    fn try_drop(&mut self, id: u64) -> Option<()> {
-        if local_remove(id).is_none() && !self.collection.contains_key(&id) {
-            return None;
-        }
-        self.cleanup_dead(id);
-        Some(())
-    }
-
-    fn create_object(&mut self, kind: ObjectKind) -> u64 {
-        let id = match kind {
-            ObjectKind::Object => self.object_id(),
-            ObjectKind::Array => self.array_id(),
-        };
-        let object = Object::new();
-        self.collection.insert(id, object.downgrade());
-        local_insert(id, object);
-        id
+fn cleanup_dead(inner: &mut InnerStorage, id: u64) {
+    let remove = inner
+        .collection
+        .get(&id)
+        .map(|w| w.strong_count() == 0)
+        .unwrap_or(false);
+    if remove {
+        inner.collection.remove(&id);
     }
 }
 
@@ -107,98 +74,162 @@ impl Storage {
     }
 
     pub fn lock(&self, id: u64) -> bool {
-        if let Some(object) = self.get_object(id) {
-            object.lock();
-            return true;
-        }
-        false
+        with_local_object(
+            id,
+            |object| {
+                object.lock();
+                true
+            },
+            || {
+                panic!("Cannot lock object that is not in local storage. Object ID: {id}");
+            },
+        )
     }
 
     pub fn unlock(&self, id: u64) -> bool {
-        if let Some(object) = self.get_object(id) {
-            object.unlock();
-            return true;
-        }
-        false
+        with_local_object(
+            id,
+            |object| {
+                object.unlock();
+                true
+            },
+            || {
+                panic!("Cannot unlock object that is not in local storage. Object ID: {id}");
+            },
+        )
     }
 
     pub fn try_lock(&self, id: u64) -> bool {
-        if let Some(object) = self.get_object(id) {
-            return object.try_lock();
-        }
-        false
+        with_local_object(
+            id,
+            |object| object.try_lock(),
+            || {
+                panic!("Cannot lock object that is not in local storage. Object ID: {id}");
+            },
+        )
     }
 
     pub fn lock_pointer(&self, id: u64) -> *const i32 {
-        if let Some(object) = self.get_object(id) {
-            return object.lock_pointer();
-        }
-        std::ptr::null()
+        with_local_object(
+            id,
+            |object| object.lock_pointer(),
+            || {
+                panic!("Cannot lock object that is not in local storage. Object ID: {id}");
+            },
+        )
     }
 
     pub fn try_drop(&self, id: u64) -> Option<()> {
+        let had_local = local_remove(id).is_some();
         let mut inner = self.inner_guard();
-        inner.try_drop(id)
+        if !had_local && !inner.collection.contains_key(&id) {
+            return None;
+        }
+        cleanup_dead(&mut inner, id);
+        Some(())
     }
 
     pub fn get_reference_count(&self, id: u64) -> Option<u32> {
-        let inner = self.inner_guard();
-        let obj = inner.collection.get(&id)?;
-        Some(obj.strong_count() as u32)
+        with_local_object(
+            id,
+            |object| Some(object.strong_count() as u32),
+            || {
+                let inner = self.inner_guard();
+                let obj = inner.collection.get(&id)?;
+                Some(obj.strong_count() as u32)
+            },
+        )
     }
 
     pub fn increment_object_references(&self, id: u64) -> Option<bool> {
-        let inner = self.inner_guard();
-        if local_get(id).is_some() {
-            return Some(true);
-        }
-
-        let weak = inner.collection.get(&id)?;
-        let object = weak.upgrade()?;
-        local_insert(id, object);
-        Some(true)
+        with_local_object(
+            id,
+            |_object| Some(true),
+            || self.get_inner_object(id).map(|_| true),
+        )
     }
 
     pub fn create_object(&self, kind: ObjectKind) -> u64 {
         let mut inner = self.inner_guard();
-        inner.create_object(kind)
+        let base_id = inner.last_id;
+        inner.last_id += 1;
+        let id = match kind {
+            ObjectKind::Object => base_id << 1,
+            ObjectKind::Array => (base_id << 1) | 0b1,
+        };
+
+        let object = Object::new();
+        inner.collection.insert(id, object.downgrade());
+        local_insert(id, object);
+        id
     }
 
     pub fn get_object(&self, id: u64) -> Option<Object> {
-        let inner = self.inner_guard();
-        inner.get_live_object(id)
+        with_local_object(
+            id,
+            |object| Some(object.clone()),
+            || self.get_inner_object(id),
+        )
     }
 
     pub fn set_object_property(&self, object_id: u64, key: u64, value: Something) {
-        let mut inner = self.inner_guard();
-        if let Some(object) = inner.get_live_object(object_id) {
-            if let Some(Something::Ref { id, .. }) = object.set_property(key, value) {
-                inner.cleanup_dead(id);
-            }
-        }
+        let v1 = value.clone();
+        let v2 = value;
+        with_local_object(
+            object_id,
+            |object| {
+                object.set_property(key, v1);
+            },
+            || {
+                let object = match self.get_inner_object(object_id) {
+                    Some(object) => object,
+                    None => return,
+                };
+                object.set_property(key, v2);
+            },
+        )
     }
 
     pub fn get_object_property(&self, object_id: u64, key: u64) -> Option<Something> {
-        let inner = self.inner_guard();
-        if let Some(object) = inner.get_live_object(object_id) {
-            return object.get_property(key);
-        }
-        None
+        with_local_object(
+            object_id,
+            |object| object.get_property(key),
+            || self.get_inner_object(object_id)?.get_property(key),
+        )
     }
 
     pub fn delete_object_property(&self, object_id: u64, key: u64) -> Option<Something> {
-        let mut inner = self.inner_guard();
-        let object = inner.get_live_object(object_id)?;
-        let prop = object.delete_property(key)?;
-        if let Something::Ref { id: to, .. } = &prop {
-            inner.cleanup_dead(*to);
-        }
+        let prop = with_local_object(
+            object_id,
+            |object| object.delete_property(key),
+            || self.get_inner_object(object_id)?.delete_property(key),
+        )?;
         Some(prop)
     }
 
-    pub fn create_reference(&self, id: u64) -> Option<Something> {
+    fn get_inner_object(&self, id: u64) -> Option<Object> {
         let inner = self.inner_guard();
-        let object = inner.get_live_object(id)?;
-        Some(Something::Ref { id, object })
+        let weak = inner.collection.get(&id)?;
+        let object = weak.upgrade()?;
+        local_insert(id, object.clone());
+        Some(object)
+    }
+
+    pub fn create_reference(&self, id: u64) -> Option<Something> {
+        with_local_object(
+            id,
+            |object| {
+                Some(Something::Ref {
+                    id,
+                    object: object.clone(),
+                })
+            },
+            || {
+                Some(Something::Ref {
+                    id,
+                    object: self.get_inner_object(id)?,
+                })
+            },
+        )
     }
 }
