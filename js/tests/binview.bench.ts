2dc40ad03fd7a01a36b49257b2a5ed965a8d5291
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-03-08 11:23:13 -0300

    binview bench

---
+++ js/tests/binview.bench.ts
@@ -0,0 +1,31 @@
+import { bench, describe } from "vitest";
+import { BinView } from "../src/BinView";
+import { SharedHeap } from "../src/AnyStore";
+import { setupFetch } from "./setupFetch";
+
+setupFetch();
+
+describe("bin view", async () => {
+  const BinViewConstructor = BinView.schema({
+    counter: "f64",
+  });
+  const db = await SharedHeap.create();
+  const obj = db.createObject({
+    view: BinViewConstructor.definition(),
+  });
+  const N = 10_000;
+  const view = obj.view;
+  view.counter = 0;
+  bench("heap counting", async () => {
+    for (let i = 0; i < N; i++) {
+      view.counter += 1;
+    }
+  });
+
+  let i = 0;
+  bench("normal js counter", async () => {
+    for (let j = 0; j < N; j++) {
+      i += 1;
+    }
+  });
+});
