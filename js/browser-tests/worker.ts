332e93dec767c0671ce9acc0612867f47b03ad99
Gabriel Machado <gabriel.delmachado@gmail.com>
2026-03-10 00:32:16 -0300

    couting

--- js/browser-tests/worker.ts
+++ js/browser-tests/worker.ts
@@ -1,18 +1,15 @@
 import { SharedHeap } from "../src/AnyStore";
-import { SharedArray } from "../src/SharedArray";
 
 self.onmessage = async (event) => {
   const { N, workerData, counterID } = event.data;
 
   const db = await SharedHeap.fromModule(workerData);
   const counter = db.getObject<{ value: number }>(counterID)!;
-  const arr = db.createObject(SharedArray.from([0]));
 
-  for (let i = 0; i < N * 100; i++) {
-    arr.set(0, arr.get(0) + 1);
-    // db.withLockOn(counter, () => {
-    // counter.value += 1;
-    // });
+  for (let i = 0; i < N; i++) {
+    db.withLockOn(counter, () => {
+      counter.value += 1;
+    });
   }
 
   self.postMessage("done");
